f6bb4a39076d4cad0ecaf77a51343402e06a3b84
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-02-19 10:52:32 +1000

    added css and js to format image gallery

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,9 @@ class App extends React.Component {
 
 	render() {
 		return (
+
 			<div className="ui container" style={{ marginTop: '10px' }}>
+				<h1 style={{textAlign: `center`}}>React Image Search App With Unsplash API</h1>
 				<SearchBar onSubmit={this.onSearchSubmit}/>
 				<ImageList images={this.state.images}/>
 			</div>
