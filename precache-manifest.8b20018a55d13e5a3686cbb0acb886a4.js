37e248fb16fd99fc56b80e64b05bd62ebe7e9c1d
Matthew Brody <matt@Matthews-MacBook-Air-3.local>
2019-02-19 14:22:33 +1000

    Updates

---
+++ precache-manifest.8b20018a55d13e5a3686cbb0acb886a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c299fc18918a5d427a",
+    "url": "/ReactAPIpics/static/js/runtime~main.30c299fc.js"
+  },
+  {
+    "revision": "9a4548901f4f23c86bd8",
+    "url": "/ReactAPIpics/static/js/main.9a454890.chunk.js"
+  },
+  {
+    "revision": "5918dbad8567dc79d88f",
+    "url": "/ReactAPIpics/static/js/2.5918dbad.chunk.js"
+  },
+  {
+    "revision": "9a4548901f4f23c86bd8",
+    "url": "/ReactAPIpics/static/css/main.0b0faee0.chunk.css"
+  },
+  {
+    "revision": "ec437c57820684c6dcf96643e2fbe462",
+    "url": "/ReactAPIpics/index.html"
+  }
+];
